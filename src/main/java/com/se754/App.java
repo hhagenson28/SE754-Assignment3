9d630bf231ca39012025f0e9f4fc325a3ce34eff
holly.hagenson <holly.hagenson@gmail.com>
2019-04-05 13:32:01 +1300

    Set up project with Maven configurations.

---
+++ src/main/java/com/se754/App.java
@@ -0,0 +1,13 @@
+package com.se754;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
